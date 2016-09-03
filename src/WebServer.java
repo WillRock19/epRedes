e82b8065721b8306f1a157c6683ea5993f7f91da
WillRock <william.pinto@lambda3.com.br>
2016-09-03 19:55:31 -0300

    Changed comments

--- src/WebServer.java
+++ src/WebServer.java
@@ -8,22 +8,23 @@ public final class WebServer
 	{
 		int port = 6789;
 		
-		//Estabelecer um socket de escuta
+		//Establish a listenning socket
 		ServerSocket mySocket = new ServerSocket(port);
 		
 		
-		// Processar a requisição de serviço HTTP em um laço infinito.
-		while (true)  {
-			// Escutar requisição de conexão TCP.
-			
+		// Process request in a HTTP service in an infinit loop
+		while (true)  
+		{
+			// Listens request of TCP connection			
 			Socket connectionSocket = mySocket.accept();
 			
-			//Construir um objeto para processar a mensagem de requisição HTTP.
+			//Build object to process HttpRequesto
 			HttpRequest request = new HttpRequest(connectionSocket);
 			
-			// Criar um novo thread para processar a requisição.
+			//Create new thread to process request
 			Thread thread = new Thread(request);
-			//Iniciar o thread.
+			
+			//Begin thread
 			thread.start();	
 		}
 	}
@@ -39,44 +40,43 @@ final class HttpRequest implements Runnable
 		this.socket = socket;
 	}
 	
-	// Implemente o método run() da interface Runnable.
+	//Implementation of run() from Runnable interface.
 	public void run()
 	{
 		try{
 			processRequest();
 		} 
 		catch (Exception e){
-			System.out.println("An error has occurred when processing the request!\n");
+			System.out.println("An error has occurred while processing the request!\n");
 			System.out.println(e);
 		}
 	}
 	
 	private void processRequest() throws Exception
 	{
-		// Obter uma referência para os trechos de entrada e saída do socket.
+		//Get references to input and output request streams
 		InputStream byteReader = socket.getInputStream();
 		DataOutputStream os = new DataOutputStream(socket.getOutputStream());
 		
-		
+		//Create line reader to read input char stream
 		BufferedReader lineReader = new BufferedReader(new InputStreamReader(byteReader));
 
 		// Read request line from HTTP request.
 		String requestLine = lineReader.readLine();
 		
-		//  Exibir a linha de requisição.
+		//Print request line
 		System.out.println();
-		System.out.println("\n\n\nA LINHA DE REQUISIÇÃO EH: \n\n");
 		System.out.println(requestLine);
 
 		
-		// Obter e exibir as linhas de cabeçalho.
+		//Get request's header lines
 		String headerLine = null;
 		while ((headerLine = lineReader.readLine()).length() != 0) 
 		{
 			System.out.println(headerLine);
 		}
 		
-		// Feche as cadeias e socket.
+		//Close writers and socket
 		os.close();
 		lineReader.close();
 		socket.close();
