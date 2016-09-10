587112d4f3f6a5247c6bb3be55d38c7bace474cc
WillRock <william.pinto@lambda3.com.br>
2016-09-10 16:03:26 -0300

    Add EP part 2 code

--- src/WebServer.java
+++ src/WebServer.java
@@ -76,11 +76,101 @@ final class HttpRequest implements Runnable
 			System.out.println(headerLine);
 		}
 		
+		// Extrair o nome do arquivo a linha de requisição.
+		StringTokenizer tokens = new StringTokenizer(requestLine);
+		
+		tokens.nextToken(); // pular o método, que deve ser “GET”
+		String fileName = tokens.nextToken();
+		
+		// Acrescente um “.” de modo que a requisição do arquivo esteja dentro do diretório atual.
+		fileName = "." + fileName;
+
+		
+		// Abrir o arquivo requisitado.
+		FileInputStream fis = null;
+		boolean fileExists = true;
+		
+		try 
+		{
+			fis = new FileInputStream(fileName);
+		} 
+		catch (FileNotFoundException e) 
+		{
+			fileExists = false;
+		}
+
+		
+		// Construir a mensagem de resposta.
+		String statusLine = null;
+		String contentTypeLine = null;
+		String entityBody = null;
+		
+		if(fileExists) 
+		{
+			statusLine = "Respondendo requisição";
+			contentTypeLine = "Content-type: " + contentType(fileName) + CRLF;
+		} 
+		else 
+		{
+			statusLine = "Página não encontrada";
+			contentTypeLine = "no content";
+			entityBody = "<HTML>" +
+				"<HEAD><TITTLE>Not Found</TITTLE></HEAD>" +
+				"<BODY>Not Found</BODY></HTML>";
+		}
+		
+		// Enviar a linha de status.
+		os.writeBytes(statusLine);
+		// Enviar a linha de tipo de conteúdo.
+		os.writeBytes(contentTypeLine);
+		// Enviar uma linha em branco para indicar o fim das linhas de cabeçalho.
+		os.writeBytes(CRLF);
+
+		// Enviar o corpo da entidade.
+		if(fileExists) 
+		{
+			sendBytes(fis, os);
+			fis.close();
+		} 
+		else 
+		{
+			os.writeBytes(entityBody);
+		}
+		
 		//Close writers and socket
 		os.close();
 		lineReader.close();
 		socket.close();
 	}
+	
+	private static void sendBytes(FileInputStream fis, OutputStream os) throws Exception
+	{
+		// Construir um buffer de 1K para comportar os bytes no caminho para o socket.
+		byte[] buffer = new byte[1024];
+		int bytes = 0;
+		
+		// Copiar o arquivo requisitado dentro da cadeia de saída do socket.
+		while((bytes = fis.read(buffer)) != -1 ) {
+			os.write(buffer, 0, bytes);
+		}
+	}
+	
+	private static String contentType(String fileName)
+	{
+		if(fileName.endsWith(".htm") || fileName.endsWith(".html")) {
+			return "text/html";
+		}
+		if(fileName.endsWith(".gif") || fileName.endsWith(".GIF"))
+		{
+			return "image/gif";
+		}
+		if(fileName.endsWith(".jpeg") || fileName.endsWith(".JPEG")) 
+		{
+			return "image/jpeg";
+		}
+		return "application/octet-stream";
+	}
+
 }
 
 
