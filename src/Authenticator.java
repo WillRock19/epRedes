f10df975bb74f587bef4ed1a17fd33624c81e552
WillRock <william.pinto@lambda3.com.br>
2016-09-25 12:12:24 -0300

    Created Base64 string tester

--- src/Authenticator.java
+++ src/Authenticator.java
@@ -1,13 +1,16 @@
 //To learn more about this decode method, see: http://www.rgagnon.com/javadetails/java-0598.html
+//To learn about the regular expression used to test if a string is in Base64, see: http://stackoverflow.com/questions/8571501/how-to-check-whether-the-string-is-base64-encoded-or-not
 
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 public class Authenticator 
 {
+	private String validCredentials = "restrict-access";
 	private String restrictUrl = "restrict-access";
 	private RequestStream requestStream;
 	private ResponseMessage messager;
+	
 	
 	public Authenticator(RequestStream requestStream)
 	{
@@ -40,7 +43,6 @@ public class Authenticator
 		
 	}
 	
-
 	
 	private boolean userHasAuthenticated()
 	{
@@ -61,15 +63,31 @@ public class Authenticator
 	
 	private boolean userCredentialsAreValid()
 	{
-		return decodeAuthentication().equals("restrict-access");
+		return decodeAuthentication().equals(validCredentials);
 	}
 	
 	private String decodeAuthentication()
 	{	
+		String[] authenticationData = requestStream.RequestHeader().Authentication().split(" ");
 		
 		
-		
-		byte[] decodedValue = Base64.getDecoder().decode(requestStream.RequestHeader().Authentication());
+		byte[] decodedValue = Base64.getDecoder().decode(getUserInformationData(authenticationData));
 		return new String(decodedValue, StandardCharsets.UTF_8);
+	}
+	
+	private String getUserInformationData(String[] authenticationData)
+	{
+		for(int i = 0; i < authenticationData.length; i++)
+		{
+			if(textIsBase64Encoded(authenticationData[i]))
+				return authenticationData[i];
+		}
+		
+		return "";
+	}
+	
+	private boolean textIsBase64Encoded(String text)
+	{
+		return text.matches("^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)$");
 	}
 }
